314772f126d12663c2ce8d847d6aeeb3d299c6ca
david <david.winiecki@gmail.com>
2014-04-03 13:54:19 -0700

    Finish level 3.

--- player.rb
+++ player.rb
@@ -1,16 +1,34 @@
 class Player
 
   def play_turn(warrior)
+    #if warrior.feel.empty?
+    #  warrior.walk!
+    #else
+    #  warrior.attack!
+    #end
+
     init warrior
     update_damage_history
-    unless rest_or_run
+    if @attacked_last_turn && warrior.feel.empty?
+      @archer = false
+      @dont_retreat = true
+    end
+    if rest_or_run
+      @attacked_last_turn = false
+    else
       if warrior.feel.empty?
-        # Rest once after every fight so we don't
-        # need to retreat as many times.
-        if @rest_since_last_fight
-          warrior.walk!
+        if @damage_history.last > 0 # Archer shot at us
+          @archer = true
+          walk!
         else
-          rest!
+          # Rest after every fight so we don't
+          # need to retreat as many times.
+          if @rest_since_last_fight
+            walk!
+          else
+            rest!
+          end
+          @attacked_last_turn = false
         end
       else
         attack!
@@ -19,23 +37,25 @@ class Player
   end
   
   def init(warrior)
+    @warrior = warrior
     @damage_history ||= []
     @full_health ||= @last_health ||= warrior.health
     if @rest_since_last_fight.nil? then @rest_since_last_fight = true end
-    @warrior = warrior
+    if @attacked_last_turn.nil? then @attacked_last_turn = false end
+    if @archer.nil? then @archer = false end
+    if @dont_retreat.nil? then @dont_retreat = false end
     # CHEAT
-    @heal_counter ||= 3
+    @heal_counter ||= 20
   end
 
   def update_damage_history
-    if @last_health - @warrior.health > 0
-      @damage_history << @last_health - @warrior.health
-    end
+    @damage_history << @last_health - @warrior.health
     @last_health = @warrior.health
   end
 
-  def attack!
-    @warrior.attack!
+  def attack!(direction = :forward)
+    @warrior.attack! direction
+    @attacked_last_turn = true
     @rest_since_last_fight = false
   end
 
@@ -48,30 +68,43 @@ class Player
     @rest_since_last_fight = true
   end
 
+  def walk!(direction = :forward)
+    @dont_retreat = false
+    @warrior.walk! direction
+  end
+
   def rest_or_run
-    return false
-    #if @warrior.health == @full_health then @target_health = nil; return false end
-    #if @target_health.nil?
-    #  if @damage_history.max >= @warrior.health
-    #    if @damage_history.max >= @full_health * 0.1
-    #      @target_health = @damage_history.max + 1#, (@damage_history.min * 2) + 1].max
-    #      @warrior.walk! :backward
-    #    else
-    #      rest!
-    #    end
-    #    return true
-    #  else
-    #    return false
-    #  end
-    #else
-    #  if @warrior.health >= @target_health
-    #    @target_health = nil
-    #    return false
-    #  else
-    #    rest!
-    #    return true
-    #  end
-    #end
+    if @archer
+      return false
+    else
+      greatest_damage_seen = @damage_history.max
+      if @warrior.health == @full_health then @target_health = nil; return false end
+      if @target_health.nil?
+        if greatest_damage_seen >= @warrior.health
+          if greatest_damage_seen >= @full_health * 0.1
+            @target_health = @damage_history.max + 1#, (@damage_history.min * 2) + 1].max
+            if @dont_retreat
+              rest!
+            else
+              walk! :backward
+            end
+          else
+            rest!
+          end
+          return true
+        else
+          return false
+        end
+      else
+        if @warrior.health >= @target_health
+          @target_health = nil
+          return false
+        else
+          rest!
+          return true
+        end
+      end
+    end
   end
 
 end
