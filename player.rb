621c74b79f0cac41e24fcdbba6719b9e2b4bdfb5
David Winiecki <david.winiecki@gmail.com>
2015-06-18 22:17:57 -0700

    Clean up

--- player.rb
+++ player.rb
@@ -1,131 +1,19 @@
-class RubyWarrior::Space
-    def contains(what)
-
-        case what
-        when :stairs
-            return self.stairs?
-        when :empty
-            return self.empty?
-        when :wall
-            return self.wall?
-        when :sludge
-            return self.to_s == 'Sludge'
-        when :thick_sludge
-            return self.to_s == 'Thick Sludge'
-        when :archer
-            return self.to_s == 'Archer'
-        when :wizard
-            return self.to_s == 'Wizard'
-        when :captive
-            return self.captive?
-        else
-            raise ArgumentError, "Space::contains does not know how to handle #{what}"
-        end
-    end
-end
 class Player
-    def init(warrior)
+    def play_turn(warrior)
 
         @warrior = warrior
         @mode ||= :first_turn
         @have_seen_both_walls_or_stairs ||= true
-        @health_last_turn ||= health
-        @attacked_last_turn ||= false
-        @rested_last_turn_after_attack ||= false
-        @first_turn = @first_turn.nil? ? true : false
 
-        if !enemy_attacked &&
-            (@rested_last_turn_after_attack || @no_enemy_attack_since_last_victory)
-            @no_enemy_attack_since_last_victory = true
-        else
-            @no_enemy_attack_since_last_victory = false
-        end
-    end
-    def rest!
-        if @attacked_last_turn
-            @rested_last_turn_after_attack = true
-        else
-            @rested_last_turn_after_attack = false
-        end
-        @attacked_last_turn = false
-        @warrior.rest!
-    end
-    def feel(direction = :forward)
-        @warrior.feel direction
-    end
-    def attack!(direction = :forward)
-        @attacked_last_turn = true
-        @rested_last_turn_after_attack = false
-        @warrior.attack!
-    end
-    def rescue!(direction = :forward)
-        @attacked_last_turn = false
-        @rested_last_turn_after_attack = false
-        @warrior.rescue! direction
-    end
-    def walk!(direction = :forward)
-        @attacked_last_turn = false
-        @rested_last_turn_after_attack = false
-        @warrior.walk! direction
-    end
-    def pivot!
-        @warrior.pivot!
-    end
-    def health
-        @warrior.health
-    end
-    def look(direction = :forward)
-        @warrior.look direction
-    end
-    def shoot!
-        @warrior.shoot!
-    end
-    # Note: this relies on enemies doing more damage (3) than the warrior can
-    # heal (2) in one turn.
-    def enemy_attacked
-        health < @health_last_turn
-    end
-    def enemy_in_range
-        #space = @warrior.look[1]
-        #if !space.empty? && !space.captive? && !space.wall? && !space.to_s == 'Sludge' && !space.to_s == 'Thick Sludge'
-        @warrior.look[1..2].each do |space|
-            #if space.contains(:archer) || space.contains(:wizard)
-            if space.contains :wizard
-                return true
-            end
-        end
-        return false
-    end
-    def see(what, direction = :forward, range = (0..-1))
-        
-        spaces = look(direction)
-        if range.class == Fixnum
-            if spaces[range].contains what
-                return true
-            else
-                return false
-            end
-        else
-            look(direction)[range].each do |space|
-                return true if space.contains what
-            end
-            return false
-        end
-    end
-    # TODO
-    # only_see([:stairs, :empty, :wall]) should be false if there is a captive on the stairs. Is that what it really returns?
-    # Even if it does, maybe the logic here should be re-done.
-    def only_see(things, direction = :forward)
-
-        things = [things] if things.class == Symbol
-
-        look(direction).each do |space|
-
-            something_else = true
-            things.each do |thing|
-                something_else = false if space.contains thing
-            end
-            return false if something_else
+        case @mode
+        when :first_turn
+            do_first_turn
+        when :fight_until_only_empty_and_wall, :fight_until_see_wall_or_stairs
+            fight_until_only_empty_and_wall
+        when :fight_to_stairs
+            fight_to_stairs
+        when :pivot_and_fight_until_only_empty_and_wall
+            pivot_and_fight_until_only_empty_and_wall
         end
     end
     def do_first_turn
@@ -181,7 +69,6 @@ class Player
         end
     end
     def fight_to_stairs
-
         advance!
     end
     def pivot_and_fight_until_only_empty_and_wall
@@ -244,155 +131,98 @@ class Player
         else
             raise "Uh oh, don't know what to do."
         end
-
-        #if feel.empty?
-        #    if enemy_in_range
-        #        shoot!
-        #    elsif @attacked_last_turn
-        #        rest!
-        #    elsif @no_enemy_attack_since_last_victory
-        #        walk!
-        #    elsif !enemy_attacked && health < 10
-        #        rest!
-        #    elsif enemy_attacked && health < 8
-        #        walk! :backward
-        #    else
-        #        walk!
-        #    end
-        #elsif feel.captive?
-        #    rescue!
-        #elsif feel.wall?
-        #    pivot!
-        #elsif health < 4
-        #    walk! :backward
-        #else
-        #    attack!
-        #end
     end
-    def play_turn(warrior)
-
-        init warrior
-
-        case @mode
-        when :first_turn
-            do_first_turn
-        when :fight_until_only_empty_and_wall, :fight_until_see_wall_or_stairs
-            fight_until_only_empty_and_wall
-        when :fight_to_stairs
-            fight_to_stairs
-        when :pivot_and_fight_until_only_empty_and_wall
-            pivot_and_fight_until_only_empty_and_wall
+    def enemy_in_range
+        look[1..2].each do |space|
+            #if space.contains(:archer) || space.contains(:wizard)
+            if space.contains :wizard
+                return true
+            end
         end
+        return false
+    end
+    def see(what, direction = :forward, range = (0..-1))
+        
+        spaces = look(direction)
+        if range.class == Fixnum
+            if spaces[range].contains what
+                return true
+            else
+                return false
+            end
+        else
+            look(direction)[range].each do |space|
+                return true if space.contains what
+            end
+            return false
+        end
+    end
+    # TODO
+    # only_see([:stairs, :empty, :wall]) should be false if there is a captive on the stairs. Is that what it really returns?
+    # Even if it does, maybe the logic here should be re-done.
+    def only_see(things, direction = :forward)
 
+        things = [things] if things.class == Symbol
 
-        #puts warrior.look[1].to_s
+        look(direction).each do |space|
 
-        #'Archer'
-        #'Wizard'
-        #'Sludge'
-        #'Thick Sludge'
-
-        # Best known for level 7, still not good enough. :p
-        # It was good enough, but -s option produces misleading victory message. :/
-        #@cheater_pumpkin_eater_actions = ['pivot!', 'walk!', 'attack!', 'attack!', 'attack!', 'attack!', 'attack!', 'walk! :backward', 'rest!', 'walk!', 'walk!', 'walk!', 'attack!', 'attack!', 'walk!', 'walk!']
-        #@action_index ||= 0
-        #eval(@cheater_pumpkin_eater_actions[@action_index])
-        #@action_index += 1
-        #return
-
-        #warrior.listen
-        #warrior.distance
-
-        # Assume stairs are in front so we want to search behind us first by
-        # default, before moving toward the stairs.
-        #if @first_turn
-        #    if feel(:backward).wall?
-        #        play_turn warrior
-        #    else
-        #        pivot!
-        #    end
-        #elsif feel.empty?
-        #    if enemy_in_range
-        #        shoot!
-        #        # 1 is archer damage (3) minus amount healed when
-        #        # resting (2).
-        #    elsif @attacked_last_turn && health > 1
-        #        rest!
-        #    elsif @no_enemy_attack_since_last_victory
-        #        walk!
-        #    elsif !enemy_attacked && health < 10
-        #        rest!
-        #    elsif enemy_attacked && health < 8
-        #        walk! :backward
-        #    else
-        #        walk!
-        #    end
-        #elsif feel.captive?
-        #rescue!
-        #elsif feel.wall?
-        #    pivot!
-        #else
-        #    attack!
-        #end
-
-
-        @health_last_turn = health
+            something_else = true
+            things.each do |thing|
+                something_else = false if space.contains thing
+            end
+            return false if something_else
+        end
+    end
+    def rest!
+        @warrior.rest!
+    end
+    def feel(direction = :forward)
+        @warrior.feel direction
+    end
+    def attack!(direction = :forward)
+        @warrior.attack!
+    end
+    def rescue!(direction = :forward)
+        @warrior.rescue! direction
+    end
+    def walk!(direction = :forward)
+        @warrior.walk! direction
+    end
+    def pivot!
+        @warrior.pivot!
+    end
+    def health
+        @warrior.health
+    end
+    def look(direction = :forward)
+        @warrior.look direction
+    end
+    def shoot!
+        @warrior.shoot!
     end
 end
+class RubyWarrior::Space
+    def contains(what)
 
-# There can be a thing on top of the stairs that hides them. E.g. on one
-# level a captive is on the stairs until you rescue them. The plan below
-# was not written with that in mind and may be disrupted by that.
-# - fight_until_only_empty_and_wall must account for the fact that stairs
-#   may be revealed on the way to the wall.
-# - the case where no stairs or walls can be seen at first must do the
-#   same.
-#
-# On the first turn, decide which direction to go and start in that
-# direction:
-#   if (stairs in view forward)
-#       if (anything other than empty and wall backward)
-#           pivot and fight_until_only_empty_and_wall
-#       else
-#           fight_to_stairs.
-#       end
-#   elsif (stairs in view backward)
-#       if (anything other than empty and wall forward)
-#           fight_until_only_empty_and_wall
-#       else
-#           pivot and fight_to_stairs
-#       end
-#   else # Don't see stairs.
-#       if (see wall in front)
-#           if (see only wall and empty in front)
-#               pivot and fight_to_stairs
-#           else
-#               fight_until_only_empty_and_wall
-#           end
-#       elsif (see wall backward)
-#           if (see only wall and empty backward)
-#               fight_to_stairs
-#           else
-#               pivot and fight_until_only_empty_and_wall
-#           end
-#       else
-#           Remember that you have not seen both walls yet
-#           fight until you see wall or stairs and then re-decide.
-#       end
-#   end
-#
-# On subsequent turns:
-#   if (haven't seen wall or stairs)
-#       if (see stairs)
-#           pivot and fight_until_only_empty_and_wall.
-#       elsif (see wall)
-#           if (see anything other than empty)
-#               fight until you see only wall or stairs.
-#           else
-#               pivot and fight_until_only_empty_and_wall.
-#           end
-#       end
-#   elsif (see wall in front and not stairs)
-#       if (only empty and wall in front)
-#           fight until you see only wall or stairs.
-#
+        case what
+        when :stairs
+            return self.stairs?
+        when :empty
+            return self.empty?
+        when :wall
+            return self.wall?
+        when :sludge
+            return self.to_s == 'Sludge'
+        when :thick_sludge
+            return self.to_s == 'Thick Sludge'
+        when :archer
+            return self.to_s == 'Archer'
+        when :wizard
+            return self.to_s == 'Wizard'
+        when :captive
+            return self.captive?
+        else
+            raise ArgumentError, "Space::contains does not know how to handle #{what}"
+        end
+    end
+end
