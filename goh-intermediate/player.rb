ad03ea2d2f9c3ccc11cdaa9a8e3fb0a405b18309
david <david.winiecki@gmail.com>
2015-06-19 14:12:08 -0700

    Pass intermediate level 2.

--- goh-intermediate/player.rb
+++ goh-intermediate/player.rb
@@ -1,5 +1,10 @@
 class Player
   def play_turn(warrior)
-      warrior.walk! warrior.direction_of_stairs
+      d = warrior.direction_of_stairs
+      if warrior.feel(d).enemy?
+          warrior.attack! d
+      else
+          warrior.walk! d
+      end
   end
 end
